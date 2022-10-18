0a2b33787ed30f826d3ef636ee162533a21d143e
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-10-18 10:17:33 +0300

    show hide button finish

--- src/component/ShowHideButton/ShowHideButton.jsx
+++ src/component/ShowHideButton/ShowHideButton.jsx
@@ -1,7 +1,16 @@
 import "./ShowHideButton.css";
-
-const ShowHideButton = () => {
-  return <button className="showHideButton">Show Add Task Bar</button>;
+const ShowHideButton = ({ showHide, setShowHide }) => {
+  const handleShowHideButton = (e) => {
+    setShowHide(!showHide);
+    showHide
+      ? (e.target.className = "hideButton")
+      : (e.target.className = "showButton");
+  };
+  return (
+    <button onClick={handleShowHideButton} className="showButton">
+      {showHide ? `Close Add Task Bar` : `Show Add Task Bar`}
+    </button>
+  );
 };
 
 export default ShowHideButton;
